7f43c5def03bc9476446527342f82e4c1c6514d4
leo <leonardogaribaydiaz@gmail.com>
2025-09-27 00:40:33 -0600

    Correcion conttraseñas almacenadas en texto plano

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { MongoClient } from "mongodb";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 const dbName = "SeguridadDB";
@@ -8,16 +9,23 @@ const dbName = "SeguridadDB";
 export async function POST(req) {
   try {
     const body = await req.json();
-    console.log("Aqui estoy")
     const { email, password } = body;
 
     await client.connect();
     const db = client.db(dbName);
     const users = db.collection("SeguridadClass");
 
-    const user = await users.findOne({ email, password });
+    const user = await users.findOne({ email });
 
     if (!user) {
+      return NextResponse.json(
+        { error: "Correo o contraseña incorrectos" },
+        { status: 401 }
+      );
+    }
+
+    const contraseniaValida = await bcrypt.compare(password, user.password);
+    if (!contraseniaValida) {
       return NextResponse.json(
         { error: "Correo o contraseña incorrectos" },
         { status: 401 }
--- src/app/api/auth/signin/route.js
+++ src/app/api/auth/signin/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { MongoClient } from "mongodb";
+import bcrypt from "bcryptjs";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 const dbName = "SeguridadDB"; 
@@ -28,10 +29,12 @@ export async function POST(req) {
         { status: 400 }
       );
     }
+    // hasheo de la contraseña en texto plano
+    const hashPassword = await bcrypt.hash(password, 10);
 
     const newUser = {
       email,
-      password, 
+      password: hashPassword, // pasamos la contraseña ya hasheada
       rol: "2", 
     };
 
